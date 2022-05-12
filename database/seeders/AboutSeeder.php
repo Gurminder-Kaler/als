091d1236fcf0d195182316681df8e109e9dcd2b1
Gurminder Kaler <gurminderkaler@Gurminders-MacBook-Air.local>
2022-05-12 10:33:07 -0400

    public/storage

---
+++ database/seeders/AboutSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\About;
+
+class AboutSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+            $about = new About();
+            $about->desc = "ALS is a non-profit, charitable organization that is concerned with the welfare of animals in Toronto. It accepts donations from individuals. It also sells products that are of interest to animal lovers. A percentage of the sales price supports ALS. ALS uses this money to support education programs, lobby government, investigate illegal activities related to animal welfare and pay employee expenses. The database is to provide access to information about welfare.";
+            $about->img =  "1652226162japheth-mast-Ga6z9QD8yvw-unsplash.jpg";    
+            $about->save();
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             // BannerSeeder::class,
-            // AdminSeeder::class,
+            // CreateAdminSeeder::class,
+            // AboutSeeder::class,
         ]);
     }
 }
