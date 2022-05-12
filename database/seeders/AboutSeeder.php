8324bd63124d0f61a1b70b82c22fa72df9c0b81d
Gurminder Kaler <gurminderkaler@Gurminders-MacBook-Air.local>
2022-05-12 10:45:06 -0400

    about seeder

--- database/seeders/AboutSeeder.php
+++ database/seeders/AboutSeeder.php
@@ -13,10 +13,10 @@ class AboutSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    {   
         //
             $about = new About();
-            $about->desc = "ALS is a non-profit, charitable organization that is concerned with the welfare of animals in Toronto. It accepts donations from individuals. It also sells products that are of interest to animal lovers. A percentage of the sales price supports ALS. ALS uses this money to support education programs, lobby government, investigate illegal activities related to animal welfare and pay employee expenses. The database is to provide access to information about welfare.";
+            $about->about = "ALS is a non-profit, charitable organization that is concerned with the welfare of animals in Toronto. It accepts donations from individuals. It also sells products that are of interest to animal lovers. A percentage of the sales price supports ALS. ALS uses this money to support education programs, lobby government, investigate illegal activities related to animal welfare and pay employee expenses. The database is to provide access to information about welfare.";
             $about->img =  "1652226162japheth-mast-Ga6z9QD8yvw-unsplash.jpg";    
             $about->save();
     }
