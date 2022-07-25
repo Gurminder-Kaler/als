486b66cc6161761283d7963e362b1e3c1a318540
Gurminder Kaler <gurminderkaler@Gurminders-MacBook-Air.local>
2022-07-25 11:25:19 -0400

    middleware fixed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,8 +56,8 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'admin' => \App\Http\Middleware\Admin::class,
-        'checkout' => \App\Http\Middleware\Checkout::class,
-        'checkAddress' => \App\Http\Middleware\CheckAddress::class,
+        'checkout' => \App\Http\Middleware\checkout::class,
+        'checkAddress' => \App\Http\Middleware\checkAddress::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
